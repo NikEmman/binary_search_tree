2def8f1fd031a2e14049fe75ff032dd8cb55b35f
NikEmman <121699424+NikEmman@users.noreply.github.com>
2023-12-28 12:04:29 +0200

    Add in_order and post_order methods

--- tree.rb
+++ tree.rb
@@ -30,6 +30,22 @@ class Tree
     pre_order(node.right)
   end
 
+  def in_order(node)
+    return if node.nil?
+
+    pre_order(node.left)
+    print "#{node.data} "
+    pre_order(node.right)
+  end
+
+  def post_order(node)
+    return if node.nil?
+
+    pre_order(node.left)
+    pre_order(node.right)
+    print "#{node.data} "
+  end
+
   def pretty_print(node = @root, prefix = '', is_left = true)
     pretty_print(node.right, "#{prefix}#{is_left ? '│   ' : '    '}", false) if node.right
     puts "#{prefix}#{is_left ? '└── ' : '┌── '}#{node.data}"
@@ -41,4 +57,7 @@ tree = Tree.new
 tree.root = tree.build_tree([20, 19, 3, 145, 67, 98, 8, 56])
 puts 'pre-order traversal of constructed BST'
 tree.pre_order(tree.root)
-tree.pretty_print
+puts 'in-order traversal of constructed BST'
+tree.in_order(tree.root)
+puts 'post-order traversal of constructed BST'
+tree.post_order(tree.root)
