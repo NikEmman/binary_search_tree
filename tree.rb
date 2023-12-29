aeef23cecb400034b5886741bf97a043f7724c0c
NikEmman <121699424+NikEmman@users.noreply.github.com>
2023-12-29 12:08:38 +0200

    Fix #delete to work regardless of children

--- tree.rb
+++ tree.rb
@@ -1,4 +1,8 @@
-require_relative "node"
+# rubocop:disable Metrics/PerceivedComplexity
+# rubocop:disable Metrics/MethodLength
+# rubocop:disable Metrics/CyclomaticComplexity
+# rubocop:disable Metrics/AbcSize
+require_relative 'node'
 
 class Tree
   attr_accessor :root
@@ -14,9 +18,9 @@ class Tree
     node
   end
 
-
   def preorder(node = @root)
     return if node.nil?
+
     temp = []
     temp << node.data
     temp << preorder(node.left)
@@ -26,6 +30,7 @@ class Tree
 
   def inorder(node = @root)
     return if node.nil?
+
     temp = []
     temp << inorder(node.left)
     temp << node.data
@@ -35,17 +40,17 @@ class Tree
 
   def postorder(node = @root)
     return if node.nil?
+
     temp = []
     temp << postorder(node.left)
     temp << postorder(node.right)
     temp << node.data
     temp.flatten.compact
   end
-  
+
   def traverse(data, node)
     temp = node
-    data < temp.data ? temp = temp.left : temp = temp.right
-    temp
+    data < temp.data ? temp.left : temp.right
   end
 
   def insert(data, node = @root)
@@ -57,38 +62,34 @@ class Tree
     data < temp.data ? temp.left = Node.new(data) : temp.right = Node.new(data)
   end
 
-  def left?(data,node)
+  def left?(data, node)
     !node.left.nil? && node.left.data == data
   end
 
-  def right?(data,node)
+  def right?(data, node)
     !node.right.nil? && node.right.data == data
   end
 
-
   def delete(data)
-    # add a clause to return error if data doesnt exist
-    #work in progress, also try using assigning grandchild_left, grandchild_right for cleaner code
+    # add a clause to return error if data doesn't exist
+    # work in progress, also try using assigning grandchild_left, grandchild_right for cleaner code
     temp = @root
-    loop do
-      temp = traverse(data,temp)
-      break if (left?(data,temp) || right?(data,temp))
-    end
-    child = traverse(data, temp)   
+    temp = traverse(data, temp) until left?(data, temp) || right?(data, temp)
+    child = traverse(data, temp)
     if child.left.nil? && child.right.nil?
-      left?(data,temp) ? temp.left = nil : temp.right = nil
+      left?(data, temp) ? temp.left = nil : temp.right = nil
     elsif child.left.nil? && !child.right.nil?
-      left?(data,temp) ? temp.left = child.right : temp.right = child.right
+      left?(data, temp) ? temp.left = child.right : temp.right = child.right
     elsif !child.left.nil? && child.right.nil?
-      left?(data,temp) ? temp.left = child.left : temp.right = child.left
+      left?(data, temp) ? temp.left = child.left : temp.right = child.left
     else
-      if left?(data,temp)
-       temp2 = child
-       temp.left = child.left
-       temp.left.right = temp2.right
+      temp2 = child
+      if left?(data, temp)
+        temp.left = temp2.left
+        temp.left.right = temp2.right
       else
-        temp2 = child
-        temp.right = child.left
+        right?(data, temp)
+        temp.right = temp2.left
         temp.right.right = temp2.right
       end
     end
@@ -103,3 +104,7 @@ end
 
 tree = Tree.new
 tree.root = tree.build_tree([20, 19, 3, 145, 67, 98, 8, 56])
+# rubocop:enable Metrics/AbcSize
+# rubocop:enable Metrics/CyclomaticComplexity
+# rubocop:enable Metrics/MethodLength
+# rubocop:enable Metrics/PerceivedComplexity
